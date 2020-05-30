b0d146d7cc264fb34dedde9a785d466d6d766538
blobbyblobfish <aywang6@gmail.com>
2020-05-30 18:03:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+let thankYouCards = [];
+
+function writeCards(friends, occasion) {
+    for (let x = 0; x < friends.length; x++)
+    thankYouCards.push(`Thank you, ${friends[x]}, for the wonderful ${occasion} gift!`);
+
+    return thankYouCards;
+}
+
+function countDown(number) {
+    while (number != 0) {
+        console.log(number);
+        number--;
+    }
+    console.log(number)
+}
