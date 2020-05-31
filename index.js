a6bbfc843aed09f2c8d8422071187832244b4b9e
Jessica <github email address>
2020-05-30 21:17:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const people = ["Lisa", "Kaitlin", "Jan"]
+let event = "birthday"
+
+function writeCards(people, event) {
+    let cards = []
+    for (let i = 0; i < people.length; i++) {
+        cards.push(`Thank you, ${people[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards;
+}
+
+function countDown(number) {
+    while (number > 0) {
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number);
+}
