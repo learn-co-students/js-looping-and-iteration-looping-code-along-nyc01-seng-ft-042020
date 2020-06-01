e694c909ba71fa8b2892b045f53e0af3b0b871c4
Srishti Nath <srishti.nath@gmail.com>
2020-05-31 22:58:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    let returnArray = [];
+    for (let index = 0; index < array.length; index++) {
+        returnArray.push(`Thank you, ${array[index]}, for the wonderful ${event} gift!`);
+    }
+    return returnArray;
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function countDown(number) {
+    let countdown = number;
+    while (countdown > 0) {
+        console.log(countdown);
+        countdown --;
+    }
+    console.log(0);
+}
+
+countDown(10);
