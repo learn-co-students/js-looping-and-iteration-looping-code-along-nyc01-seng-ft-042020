4bdbc7a897d31f1d5a24a06938fe7b6fac32b48f
DarkLaughter <ramon.l.echeverria@gmail.com>
2020-05-31 21:20:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards( array, element ) {
+    let thankYou = []
+    for ( let i = 0; i < array.length; i++ ) {
+        const name = array[i];
+      thankYou.push( `Thank you, ${name}, for the wonderful ${element} gift!` )
+    }
+    return thankYou
+  }
+
+  function countDown( num ) {
+    while ( num > 0 ) {
+      console.log( num );
+      num -= 1;
+    }
+    console.log( num );
+  }
