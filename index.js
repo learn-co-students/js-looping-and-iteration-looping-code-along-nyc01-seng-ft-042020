7a460fcda365e6c770ddeb314e8e1945e85e0b59
carolineglass <carolineglass89@gmail.com>
2020-05-31 20:37:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(name, event) {
+    let array = []
+    for (let i = 0; i < name.length; i++){
+        array.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return array;
+}
+
+let countdown = 0
+function countDown(number) {
+    while (countdown <= number) {
+        console.log(number--);
+    }
+}
+    
