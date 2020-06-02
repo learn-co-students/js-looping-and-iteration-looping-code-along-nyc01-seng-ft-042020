aa08abf619c224305b95f81d093d22259ff07f06
byoon97 <byoon5397@gmail.com>
2020-06-01 23:00:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(nameArray, eventName) {
+    const thankYouCards = []
+        for (let i = 0; i < nameArray.length; i += 1) {
+            thankYouCards.push(`Thank you, ${nameArray[i]}, for the wonderful ${eventName} gift!`)
+         }
+    return thankYouCards
+}
+
+function countDown(startingNumber) {
+    while (startingNumber > 0) {
+        console.log(startingNumber);
+        startingNumber -=1;
+    }
+
+    console.log(startingNumber);
+}
+
